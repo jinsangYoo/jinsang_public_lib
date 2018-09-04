071fb28f89f4725e8faa0a0b5789d7cb73fead54
유진상/데이터운영지원팀/AS <jinsang@nhnent.com>
2018-09-04 17:00:29 +0900

    add ios lib cocoapods project

---
+++ Example/Pods/Target Support Files/Pods-jinsang_public_lib_Tests/Pods-jinsang_public_lib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jinsang_public_lib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_jinsang_public_lib_Tests
+@end
---
+++ Example/Pods/Target Support Files/jinsang_public_lib/jinsang_public_lib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_jinsang_public_lib : NSObject
+@end
+@implementation PodsDummy_jinsang_public_lib
+@end

af7f65bb9d17045afc7081105bae41684d756bf2
유진상/데이터운영지원팀/AS <jinsang@nhnent.com>
2018-09-05 18:36:56 +0900

    프로젝트 삭제

--- Example/Pods/Target Support Files/Pods-jinsang_public_lib_Example/Pods-jinsang_public_lib_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_jinsang_public_lib_Example : NSObject
-@end
-@implementation PodsDummy_Pods_jinsang_public_lib_Example
-@end
--- Example/Pods/Target Support Files/jinsang_public_lib/jinsang_public_lib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_jinsang_public_lib : NSObject
-@end
-@implementation PodsDummy_jinsang_public_lib
-@end
